b0a60e6643ba6869ebe76905c67bb1610b093dd2
Roberto Machorro <7190436+RobertoMachorro@users.noreply.github.com>
2019-08-11 17:35:31 -0400

    Updated source banners to reflect GPL licensing. All new files will also use the same banner.

--- MacPad/AppDelegate.swift
+++ MacPad/AppDelegate.swift
@@ -1,9 +1,21 @@
 //
 //  AppDelegate.swift
-//  MacPad
 //
-//  Created by Roberto Machorro on 8/9/19.
-//  Copyright © 2019 Unplugged Ideas, LLC. All rights reserved.
+//  MacPad - A general purpose text editor, small and light.
+//  Copyright © 2019 Roberto Machorro. All rights reserved.
+//
+//	This program is free software: you can redistribute it and/or modify
+//	it under the terms of the GNU General Public License as published by
+//	the Free Software Foundation, either version 3 of the License, or
+//	(at your option) any later version.
+//
+//	This program is distributed in the hope that it will be useful,
+//	but WITHOUT ANY WARRANTY; without even the implied warranty of
+//	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//	GNU General Public License for more details.
+//
+//	You should have received a copy of the GNU General Public License
+//	along with this program.  If not, see <https://www.gnu.org/licenses/>.
 //
 
 import Cocoa
