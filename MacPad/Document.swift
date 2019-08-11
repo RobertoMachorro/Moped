cc85046ecb6ff7388507b6dc503603011990901f
Roberto Machorro <7190436+RobertoMachorro@users.noreply.github.com>
2019-08-11 17:42:08 -0400

    Implemented model (StringModel) on Document. This includes basic read/write and printing. Connected the model to viewController via representedObject, this can be used in Storyboard views. Setup async i/o for Document.

--- MacPad/Document.swift
+++ MacPad/Document.swift
@@ -1,44 +1,114 @@
 //
 //  Document.swift
-//  MacPad
 //
-//  Created by Roberto Machorro on 8/11/19.
+//  MacPad - A general purpose text editor, small and light.
 //  Copyright © 2019 Roberto Machorro. All rights reserved.
+//
+//	This program is free software: you can redistribute it and/or modify
+//	it under the terms of the GNU General Public License as published by
+//	the Free Software Foundation, either version 3 of the License, or
+//	(at your option) any later version.
+//
+//	This program is distributed in the hope that it will be useful,
+//	but WITHOUT ANY WARRANTY; without even the implied warranty of
+//	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//	GNU General Public License for more details.
+//
+//	You should have received a copy of the GNU General Public License
+//	along with this program.  If not, see <https://www.gnu.org/licenses/>.
 //
 
 import Cocoa
 
 class Document: NSDocument {
 
+	@objc let model = StringModel(textString: "")
+
 	override init() {
-	    super.init()
-		// Add your subclass-specific initialization here.
+		super.init()
 	}
 
+	// MARK: - Enablers
+
 	override class var autosavesInPlace: Bool {
+		return true // FALSE: Enables Save As, disables versioning
+	}
+
+	override func canAsynchronouslyWrite(to url: URL, ofType typeName: String, for saveOperation: NSDocument.SaveOperationType) -> Bool {
 		return true
 	}
+
+	override class func canConcurrentlyReadDocuments(ofType: String) -> Bool {
+		switch ofType {
+		case "public.plain-text", "public.source-code", "public.script":
+			return true
+		default:
+			return false
+		}
+	}
+
+	// MARK: - User Interface
 
 	override func makeWindowControllers() {
 		// Returns the Storyboard that contains your Document window.
 		let storyboard = NSStoryboard(name: NSStoryboard.Name("Main"), bundle: nil)
-		let windowController = storyboard.instantiateController(withIdentifier: NSStoryboard.SceneIdentifier("Document Window Controller")) as! NSWindowController
-		self.addWindowController(windowController)
+		if let windowController = storyboard.instantiateController(withIdentifier: NSStoryboard.SceneIdentifier("Document Window Controller")) as? WindowController {
+			self.addWindowController(windowController)
+
+			if let viewController = windowController.contentViewController as? ViewController {
+				viewController.representedObject = model
+			}
+		}
+	}
+
+	// MARK: - Reading and Writing
+
+	override func read(from data: Data, ofType typeName: String) throws {
+		model.read(from: data)
 	}
 
 	override func data(ofType typeName: String) throws -> Data {
-		// Insert code here to write your document to data of the specified type, throwing an error in case of failure.
-		// Alternatively, you could remove this method and override fileWrapper(ofType:), write(to:ofType:), or write(to:ofType:for:originalContentsURL:) instead.
-		throw NSError(domain: NSOSStatusErrorDomain, code: unimpErr, userInfo: nil)
+		return model.data()!
 	}
 
-	override func read(from data: Data, ofType typeName: String) throws {
-		// Insert code here to read your document from the given data of the specified type, throwing an error in case of failure.
-		// Alternatively, you could remove this method and override read(from:ofType:) instead.
-		// If you do, you should also override isEntireFileLoaded to return false if the contents are lazily loaded.
-		throw NSError(domain: NSOSStatusErrorDomain, code: unimpErr, userInfo: nil)
+	// MARK: - Printing
+
+	func thePrintInfo() -> NSPrintInfo {
+		let thePrintInfo = NSPrintInfo()
+		thePrintInfo.horizontalPagination = .fit
+		thePrintInfo.isHorizontallyCentered = false
+		thePrintInfo.isVerticallyCentered = false
+		
+		// One inch margin all the way around.
+		thePrintInfo.leftMargin = 72.0
+		thePrintInfo.rightMargin = 72.0
+		thePrintInfo.topMargin = 72.0
+		thePrintInfo.bottomMargin = 72.0
+
+		printInfo.dictionary().setObject(NSNumber(value: true), forKey: NSPrintInfo.AttributeKey.headerAndFooter as NSCopying)
+		
+		return thePrintInfo
 	}
 
+	@objc func printOperationDidRun(_ printOperation: NSPrintOperation, success: Bool, contextInfo: UnsafeMutableRawPointer?) {
+		// Printing finished...
+	}
+
+	@IBAction override func printDocument(_ sender: Any?) {
+		// Print the NSTextView.
+
+		// Create a copy to manipulate for printing.
+		let pageSize = NSSize(width: (printInfo.paperSize.width), height: (printInfo.paperSize.height))
+		let textView = NSTextView(frame: NSRect(x: 0.0, y: 0.0, width: pageSize.width, height: pageSize.height))
+
+		// Make sure we print on a white background.
+		textView.appearance = NSAppearance(named: .aqua)
+
+		// Copy the attributed string.
+		textView.textStorage?.append(NSAttributedString(string: model.textString))
+
+		let printOperation = NSPrintOperation(view: textView)
+		printOperation.runModal(for: windowControllers[0].window!, delegate: self, didRun: #selector(printOperationDidRun(_:success:contextInfo:)), contextInfo: nil)
+	}
 
 }
-
