cc8de2aceba71c95d53035c6289f545f69e8567e
Roberto Machorro <7190436+RobertoMachorro@users.noreply.github.com>
2019-08-11 16:01:42 -0400

    Created Xcode templated project. Swift & Storyboard based, Document-Based Application.

---
+++ MacPad/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MacPad
+//
+//  Created by Roberto Machorro on 8/11/19.
+//  Copyright © 2019 Roberto Machorro. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+	func applicationDidFinishLaunching(_ aNotification: Notification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(_ aNotification: Notification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
---
+++ MacPad/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MacPad
+//
+//  Created by Roberto Machorro on 8/11/19.
+//  Copyright © 2019 Roberto Machorro. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: Any? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
