8e0ac345bfa31a6e8ea4b15ebfa384a49150cd51
Roberto Machorro <7190436+RobertoMachorro@users.noreply.github.com>
2019-08-23 21:29:36 -0400

    Preferences model, uses UserDefaults, KVO enabled for Cocoa bindings.

---
+++ Moped/Preferences.swift
@@ -0,0 +1,81 @@
+//
+//  Preferences.swift
+//
+//  Moped - A general purpose text editor, small and light.
+//  Copyright © 2019 Roberto Machorro. All rights reserved.
+//
+//	This program is free software: you can redistribute it and/or modify
+//	it under the terms of the GNU General Public License as published by
+//	the Free Software Foundation, either version 3 of the License, or
+//	(at your option) any later version.
+//
+//	This program is distributed in the hope that it will be useful,
+//	but WITHOUT ANY WARRANTY; without even the implied warranty of
+//	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//	GNU General Public License for more details.
+//
+//	You should have received a copy of the GNU General Public License
+//	along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+
+import Foundation
+
+class Preferences: NSObject {
+
+	// MARK: - Singleton with Custom Setup
+
+	static let userShared: Preferences = {
+		let shared = Preferences()
+		return shared
+	}()
+
+	// MARK: - Preferences / Properties
+
+	@objc dynamic var language: String {
+		get {
+			return getStringValue(forKey: "language", otherwiseUse: "plaintext")
+		}
+		set {
+			setStringValue(forKey: "language", to: newValue)
+		}
+	}
+
+	@objc dynamic var theme: String {
+		get {
+			return getStringValue(forKey: "theme", otherwiseUse: "xcode")
+		}
+		set {
+			setStringValue(forKey: "theme", to: newValue)
+		}
+	}
+
+	@objc dynamic var font: String {
+		get {
+			return getStringValue(forKey: "font", otherwiseUse: "Menlo")
+		}
+		set {
+			setStringValue(forKey: "font", to: newValue)
+		}
+	}
+
+	@objc dynamic var fontSize: String {
+		get {
+			return getStringValue(forKey: "fontSize", otherwiseUse: "13")
+		}
+		set {
+			setStringValue(forKey: "fontSize", to: newValue)
+		}
+	}
+
+	// MARK: - UserDefaults Helpers
+
+	func getStringValue(forKey key: String, otherwiseUse backup: String) -> String {
+		return UserDefaults.standard.string(forKey: key) ?? backup
+	}
+
+	func setStringValue(forKey key: String, to value: String) {
+		UserDefaults.standard.set(value, forKey: key)
+		NotificationCenter.default.post(name: Notification.Name(rawValue: "PreferencesChanged"), object: nil)
+	}
+
+}
