34e1e998f93638c22794a23dea63f9dc606a5601
Roberto Machorro <7190436+RobertoMachorro@users.noreply.github.com>
2022-03-13 23:25:25 -0400

    Created NSTextView inherited wrapper class.

---
+++ Moped/WrapperTextView.swift
@@ -0,0 +1,17 @@
+//
+//  WrapperTextView.swift
+//  Moped
+//
+//  Created by Roberto Machorro on 3/13/22.
+//  Copyright © 2022 Roberto Machorro. All rights reserved.
+//
+
+import Cocoa
+
+class WrapperTextView: NSTextView {
+
+	override func draw(_ dirtyRect: NSRect) {
+		super.draw(dirtyRect)
+	}
+
+}
